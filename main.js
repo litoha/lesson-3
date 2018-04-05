31279a5ae4216dd0ea3f18f81f63e38ef793882d
Natasha <nata-nik.92@mail.ru>
2018-04-05 21:42:52 +0300

    lesson 3

---
+++ main.js
@@ -0,0 +1,59 @@
+//*1* 
+//  
+//var arr = ['Есть', 'жизнь', 'на', 'Марсе'];
+//
+//var arrLength = arr.map(function(item) {
+//  return item.length;
+//});
+//
+//alert (arrLength); 
+//
+
+//*2*
+//
+//var a = prompt ('Логин?');
+//  if (a == 'Админ') {
+//    var b = prompt ('Пароль?');
+//    if (b == 'Черный властелин') {
+//      alert ('Добро пожаловать!');
+//    } else if (b == null) {
+//      alert ('Вход отменен');      
+//    } else {
+//      alert ('Пароль неверен');
+//    }
+//  } else if (a == null) {
+//    alert ('Вход отменен');
+//  }  else {
+//    alert ('Я васне знаю');
+//  }
+
+//*3*
+//
+//var message;
+//var message = (login === 'Вася') ? 'Привет' :
+//(login === 'Директор') ? 'Здравствуйте' :
+//(login === ' ') ? 'Нет логина' :
+//' ';
+
+//*4*
+//  
+//var i;
+//for (i = 2; i <= 10; i++) {
+//  if (i%2 == 0) {
+//    alert (i);
+//  }
+//}
+
+//*5*
+//var arr = [ ];
+//while (true) {
+//  var a = prompt ('Введите число');
+//  if (a == '' || a == null) 
+//  break;
+//  arr.push (+a);
+//}
+//var sum = 0;
+//for (var i = 0; i < arr.length; i++) {
+//    sum += arr[i];
+//}
+//  alert (sum);
